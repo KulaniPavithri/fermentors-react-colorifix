5b8e6b37d2835bd3606e9750d0590c63d7427a0c
Kulani Pavithri <kulani.pavithri@gmail.com>
2022-10-29 00:35:49 +0100

    change BrowserRouter to HashRouter

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import FermentationList from "./FermentationList";
 import Fermentors from "./Fermentors";
@@ -8,14 +8,14 @@ import Viewgraph from "./Viewgraph";
 const App = () => {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Header />
             <Routes>
                 <Route exact path="/" element={<Fermentors />} />
                 <Route exact path="/fermentations" element={<FermentationList />} />
                 <Route exact path="/fermentationgraph" element={<Viewgraph />} />           
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
   
 };
